f9b4b2c512e4b57f100e73adcbf3142140065e7d
kenwu <kenqiwu@gmail.com>
2025-11-20 23:28:29 -0800

    Delete unused files

--- src/modules/gameResourcesDock/components/index.ts
+++ src/modules/gameResourcesDock/components/index.ts
@@ -1,4 +1,2 @@
-export { SearchBar } from './SearchBar';
-export { SortControl } from './SortControl';
 export { PileViewer } from './PileViewer';
 export type { PileViewerCallbacks, PileType } from './PileViewer';
