99616273af5b3f1b1b36e16c5a2fd22c59ff430d
kenwu <kenqiwu@gmail.com>
2025-11-27 15:20:08 -0800

    Add scry and add card tests

---
+++ tests/e2e/addCard.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from './fixtures';
+
+
+test.beforeEach(async ({page, context}) => {
+  await page.goto('/', {waitUntil: 'networkidle'});
+  await page.evaluate(() => localStorage.clear());
+});
+
+test('testAddCardAddsACard', async ({page}) => {
+  await page.getByRole('button', { name: 'Add Card' }).click();
+  await page.getByRole('textbox', { name: 'Enter the exact card name' }).fill('The Ultimate Nightmare of Wizards of the Coast® Customer Service');
+  await page.getByRole('button', { name: 'Add to Hand' }).click();
+  await page.getByAltText('The Ultimate Nightmare of Wizards of the Coast® Customer Service').waitFor({state: 'visible'});
+})
+
+test('testAddCardAddsACardUsingEnterKey', async ({page}) => {
+  await page.getByRole('button', { name: 'Add Card' }).click();
+  await page.getByRole('textbox', { name: 'Enter the exact card name' }).fill('The Ultimate Nightmare of Wizards of the Coast® Customer Service');
+  await page.getByRole('textbox', { name: 'Enter the exact card name' }).press('Enter');
+  await page.getByAltText('The Ultimate Nightmare of Wizards of the Coast® Customer Service').waitFor({state: 'visible'});
+})
