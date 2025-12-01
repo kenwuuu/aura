9a24fc71dea7de5e9220a71617bc28c7e190bc1c
kenwu <kenqiwu@gmail.com>
2025-11-30 20:19:38 -0800

    Add basic tooltip tests

---
+++ tests/e2e/app/board/card_tooltips.spec.ts
@@ -0,0 +1,44 @@
+import {expect, test} from "../../fixtures";
+import {Page} from "playwright/test";
+import {Locator} from "@playwright/test";
+
+async function dragHandCardToLocator(locator: Locator, page: Page) {
+  await page.locator('div').filter({hasText: '#'}).nth(4).dragTo(locator);
+  await expect(page.locator('div').filter({hasText: '#'}).nth(3)).toBeVisible();
+}
+
+test('testExileTooltip', async ({ page }) => {
+  expect(page.getByText('Exile0'));
+
+  await dragHandCardToLocator(page.locator('#whiteboard'), page);
+  const boardCard = page.locator('div').filter({ hasText: '#' }).nth(3);
+  await boardCard.click();
+  const tooltipRow = page.getByText('SExile');
+  await tooltipRow.waitFor({ state: 'visible' });
+  await tooltipRow.click();
+  await expect(page.getByText('Exile1')).toBeVisible();
+});
+
+test('testDiscardTooltip', async ({ page }) => {
+  expect(page.getByText('Discard0'));
+
+  await dragHandCardToLocator(page.locator('#whiteboard'), page);
+  const boardCard = page.locator('div').filter({ hasText: '#' }).nth(3);
+  await boardCard.click();
+  const tooltipRow = page.getByText('DDiscard');
+  await tooltipRow.waitFor({ state: 'visible' });
+  await tooltipRow.click();
+  await expect(page.getByText('Discard1')).toBeVisible();
+});
+
+test('testHandTooltip', async ({ page }) => {
+  expect(page.getByText('Hand92'));
+
+  await dragHandCardToLocator(page.locator('#whiteboard'), page);
+  const boardCard = page.locator('div').filter({ hasText: '#' }).nth(3);
+  await boardCard.click();
+  const tooltipRow = page.getByText('HHand');
+  await tooltipRow.waitFor({ state: 'visible' });
+  await tooltipRow.click();
+  await expect(page.getByText('Hand93')).toBeVisible();
+});
