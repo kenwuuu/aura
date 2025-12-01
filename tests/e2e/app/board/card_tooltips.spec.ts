795360d55f058c1e0f17ba0bcb66609ce744220f
kenwu <kenqiwu@gmail.com>
2025-11-30 21:58:55 -0800

    Add test for interactive board card tooltip

--- tests/e2e/app/board/card_tooltips.spec.ts
+++ tests/e2e/app/board/card_tooltips.spec.ts
@@ -7,6 +7,22 @@ async function dragHandCardToLocator(locator: Locator, page: Page) {
   await expect(page.locator('div').filter({hasText: '#'}).nth(3)).toBeVisible();
 }
 
+export async function getElementOrientation(
+  locator: Locator
+): Promise<"portrait" | "landscape" | "square"> {
+  const box = await locator.boundingBox();
+
+  if (!box) {
+    throw new Error("Element not found or not visible.");
+  }
+
+  const { width, height } = box;
+
+  if (width > height) return "landscape";
+  if (height > width) return "portrait";
+  return "square";
+}
+
 test('testExileTooltip', async ({ page }) => {
   expect(page.getByText('Exile0'));
 
@@ -45,11 +61,11 @@ test('testDeckTooltip', async ({ page }) => {
 });
 
 test('testHandTooltip', async ({ page }) => {
-  const eighthBoardCard = page.locator('.hand-cards .hand-card').nth(7);
-  await expect(eighthBoardCard).toBeVisible();
+  const eighthHandCard = page.locator('.hand-cards .hand-card').nth(7);
+  await expect(eighthHandCard).toBeVisible();
 
   await dragHandCardToLocator(page.locator('#whiteboard'), page);
-  await expect(eighthBoardCard).toBeHidden();
+  await expect(eighthHandCard).toBeHidden();
 
   const boardCard = page.locator('div').filter({ hasText: '#' }).nth(3);
   await boardCard.click();
@@ -57,5 +73,58 @@ test('testHandTooltip', async ({ page }) => {
   await tooltipRow.waitFor({ state: 'visible' });
   await tooltipRow.click();
 
-  await expect(eighthBoardCard).toBeVisible();
+  await expect(eighthHandCard).toBeVisible();
+});
+
+test('testInteractiveTooltip', async ({ page }) => {
+  const firstBoardCard = page.locator('.player-board .card').nth(0);
+  const secondBoardCard = page.locator('.player-board .card').nth(1);
+  const thirdBoardCard = page.locator('.player-board .card').nth(2);
+  
+  await dragHandCardToLocator(page.locator('#whiteboard'), page);
+  
+  expect(await getElementOrientation(firstBoardCard)).toBe('portrait');
+
+  await firstBoardCard.click();
+  await page.getByText('Tap/Untap').click();
+  expect(await getElementOrientation(firstBoardCard)).toBe('landscape');
+
+  await firstBoardCard.click();
+  await page.getByText('XUntap all').click();
+  expect(await getElementOrientation(firstBoardCard)).toBe('portrait');
+
+  await firstBoardCard.click();
+  await page.getByText('FFlip').click();
+  const cardImgSrc = await firstBoardCard.locator('img').getAttribute('src');
+  expect(cardImgSrc === '/assets/card-back.png')
+
+  await firstBoardCard.click();
+  await page.getByText('U+1 counter').click();
+  await page.getByTitle('Decrease counter').waitFor({ state: 'visible' });
+  await page.getByTitle('Decrease counter').click();
+
+  await firstBoardCard.click();
+  await page.getByText('I-1 counter').click();
+  await page.getByTitle('Increase counter').waitFor({ state: 'visible' });
+  await page.getByTitle('Increase counter').click();
+
+  // copy first card to make second card.
+  // copy second card to make third card
+  await firstBoardCard.click();
+  await page.getByText('KCopy/clone').click();
+  await secondBoardCard.waitFor({ state: 'visible' });
+  await secondBoardCard.click();
+  await page.getByText('KCopy/clone').click();
+  await thirdBoardCard.waitFor({ state: 'visible' });
+  await expect(thirdBoardCard).toBeVisible();
+
+  // delete third card
+  await thirdBoardCard.click();
+  await page.getByText('BackDelete').click();
+  await expect(thirdBoardCard).toBeHidden();
+
+  // move second card to hand
+  await secondBoardCard.click();
+  await page.getByText('HHand').click();
+  await expect(secondBoardCard).toBeHidden();
 });
