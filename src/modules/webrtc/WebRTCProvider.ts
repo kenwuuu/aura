bce4b7e943f98438cabfffc98f439653fcc335b6
kenwu <kenqiwu@gmail.com>
2025-12-02 23:04:24 -0800

    Change wss URL to aura0

--- src/modules/webrtc/WebRTCProvider.ts
+++ src/modules/webrtc/WebRTCProvider.ts
@@ -101,7 +101,7 @@ export class WebRTCProvider {
 
   static async createWsYjs(yDoc: Y.Doc, config: WebRTCConfig): Promise<WebsocketProvider> {
     return new WebsocketProvider(
-      'wss://3.151.83.118',
+      'wss://aura0.app',
       config.roomName,
       yDoc,
     );
